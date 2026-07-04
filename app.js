344ac1514c7b4611414d40a366a19b6dde908e38
Javier E <jembfh@gmail.com>
2026-07-05 01:32:19 +0200

    Intento sin éxito de conectar API REST

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ import cors from 'cors';
 import productsRoutes from './routes/products.routes.js';
 
 const app = express();
+
+
+// app.use(cors());
 app.use(cors());
+
 app.use(express.json());
 app.use('/api/products', productsRoutes);
 
 app.listen(3000, () => {
     console.log('API LISTA EN PUERTO 3000');
-});
+    }
+);
 
